378ec742fc89a7ef877c86ce7e2737a41f4bb8b3
Emilio Barrachina Gascó <s182804@dtu.dk>
2025-09-17 17:13:04 +0200

    Extend code for sorting the numbers of the list

--- Module03/Task03extra_outputs.py
+++ Module03/Task03extra_outputs.py
@@ -29,6 +29,20 @@ if numbers: # Tells how to proceed when the numbers list is not empty
 	print(f"The list of numbers is: {numbers}") # Printing the list of numbers
 	print(f"The minimal number entered is: {minimal}")
 	print(f"The maximal number entered is: {maximal}")
+        
+        # Sorting the list of numbers entered by the user
+        sort_numbers = True  
 
+        # Implementing a sorting algorithm
+        n = len(numbers) # Getting the total number of elements within the number list
+                         # Storing the total number in the variable n defined   
+        for i in range(n): # Iterating n times, placing sorted at least one element for each pass
+                           
+            for j in range(0, n - i - 1): # Iteraring through the unsorted elements of the list
+                if sort_numbers:
+                     if numbers[j] > numbers[j + 1]:
+                        numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+        print(f"The sorted list of numbers is: {numbers}")
 else: # Printing message when there were no numbers entered
     print("No numbers were entered.")
