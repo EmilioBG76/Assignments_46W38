bb1ded05ca58699be73e6b1c7254f8c4e97dd572
Emilio Barrachina Gascó <s182804@dtu.dk>
2025-09-28 15:34:52 +0200

    Add python file for using functions defined previously in MyModul.py file

---
+++ Module04/task_one.py
@@ -0,0 +1,11 @@
+# Importing from MyModule file the functions defined previosly
+from MyModule import get_a_list_of_numbers, find_max, find_min
+
+# Getting the list of numbers
+numbers_list = get_a_list_of_numbers()
+
+# Finding the minimum value of the list entered
+min_in_list = find_min(numbers_list)
+
+# Finding the maximum value of the list entered
+max_in_list = find_max(numbers_list)
