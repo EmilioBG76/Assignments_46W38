461c69fada836ecbaf2d3e2fb14c55b03bc1cc10
Emilio Barrachina Gascó <s182804@dtu.dk>
2025-10-06 11:44:16 +0200

    Correct a typo

--- Module06/Task1.py
+++ Module06/Task1.py
@@ -10,7 +10,7 @@ pie_labels = ['Crude Oil', 'Natural gas', 'Renew. Energy', 'Solid fuels', 'Nucle
 pie_sizes = [37.7, 20.4, 19.5, 10.6, 11.8]
 pie_colors = ['red', 'blue', 'green', 'orange', 'yellow']
 
-plt.pi(pie_sizes, labels=pie_labels, colors=pie_colors, autopct='%1.1f%%', startangle=90)
+plt.pie(pie_sizes, labels=pie_labels, colors=pie_colors, autopct='%1.1f%%', startangle=90)
 plt.title('Energy generation sources within the EU in 2023')
 
 plt.show()
