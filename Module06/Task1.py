dcab92ae4f26a565cb0b796be9c93b33a7c6e516
Emilio Barrachina Gascó <s182804@dtu.dk>
2025-10-06 11:13:30 +0200

    Add some extra line to the code for task1

--- Module06/Task1.py
+++ Module06/Task1.py
@@ -1,6 +1,6 @@
 # Module 06 - task 1 - Pie Chart
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(9,7))
 ax = fig.add_subplot()
@@ -9,5 +9,8 @@ ax = fig.add_subplot()
 pie_labels = ['Crude Oil', 'Natural gas', 'Renew. Energy', 'Solid fuels', 'Nuclear Energy']
 pie_sizes = [37.7, 20.4, 19.5, 10.6, 11.8]
 pie_colors = ['red', 'blue', 'green', 'orange', 'yellow']
+
+plt.pi(pie_sizes, labels=pie_labels, colors=pie_colors, autopct='%1.1f%%', startangle=90)
+plt.title('Energy generation sources within the EU in 2023')
 
 plt.show()
