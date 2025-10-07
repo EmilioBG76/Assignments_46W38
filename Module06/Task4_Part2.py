3ea52a20c293b6d507e974f3091d5d44bc8ef515
Emilio Barrachina Gascó <s182804@dtu.dk>
2025-10-07 16:12:59 +0200

    Adding plottings for task4 part2 to previous version file

--- Module06/Task4_Part2.py
+++ Module06/Task4_Part2.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import math
+
 
 # Loading excel file from Exercise 4” sheet of the “Module 6 – Exercises Data
 
@@ -15,49 +15,57 @@ df = pd.read_excel(data_fileex4, sheet_name="Exercise 4 - Baseline", engine='ope
 df_A = pd.read_excel(data_fileex4, sheet_name="Exercise 4 - A", engine='openpyxl')
 df_B = pd.read_excel(data_fileex4, sheet_name="Exercise 4 - B", engine='openpyxl')
 
-# Calculating the standar deviations for every single wind turbine and their data
-std_dev_Baseline = df.std()
-std_dev_A = df_A.std()
-std_dev_B = df_B.std()
+# Selecting the relevant output metrics for standard deviation calculation
+output_metrics = ['Blade pitch (degrees)', 'Rotor speed (rpm)', 'Platform pitch\
+ (degrees)', 'Thrust (kN)', 'Tower base moment (kNm)']
 
-# Printing the standar deviation results for all wind turbine datas
+# Calculating the standard deviations for every single wind turbine and their data
+std_devs = {}
 
-#print("Standard deviations:")
-#print(f'For turbine "Baseline" the standard deviation are {std_dev_Baseline}')
-#print(f'For turbine "A" the standard deviation are {std_dev_A}')
-#print(f'For turbine "B" the standard deviation are {std_dev_B}')
+std_devs['Baseline'] = df[output_metrics].std()
+std_devs['Turbine A'] = df_A[output_metrics].std()
+std_devs['Turbine B'] = df_B[output_metrics].std()
 
-# Creating a dictionary to hold the standard deviations calculated for each WT
+# Creating a DataFrame with the dictionary for absolute standard deviations of WTs
+std_devs_df = pd.DataFrame(std_devs).T
 
-std_dev_WTdata = {'Baseline': std_dev_Baseline,
-                'Turbine A': std_dev_A,
-                'Turbine B': std_dev_B}
-
-# Creating a DataFrame with the dictionary
-
-std_dev_df = pd.DataFrame(std_dev_WTdata)
-
-# Printing the results in DataFrame format
-#print("The standard deviations calculated for each wind turbine are:")
-#print(std_dev_df)
+# Printing absolute standard deviations for each WT
+print("The absolute standard deviations calculated for each wind turbine are:")
+print(f'{std_devs_df}')
 
 # Calculating normalized standard deviations for each WT data provided
+norm_std_devs = {}
+baseline_std_devs = std_devs_df.loc['Baseline']
 
-norm_std_dev_A = std_dev_A / std_dev_Baseline
-#print(f'The normalized st. deviation values for Turbine A are: {norm_std_dev_A}')
+norm_std_devs['Turbine A'] = std_devs_df.loc['Turbine A'] / baseline_std_devs
+norm_std_devs['Turbine B'] = std_devs_df.loc['Turbine B'] / baseline_std_devs
 
-norm_std_dev_B = std_dev_B / std_dev_Baseline
-#print(f'The normalized st. deviation values for Turbine B are: {norm_std_dev_B}')
+# Creating a new DataFrame to hold normalized WTs standard deviations calculated
+norm_std_devs_df = pd.DataFrame(norm_std_devs).T
 
-# Creating a new dictionary to hold normalized WTs standard deviations calculated 
-
-norm_std_dev_WTdata = {'Baseline': std_dev_Baseline,
-                    'Turbine A': norm_std_dev_A,
-                    'Turbine B': norm_std_dev_B}
-
-# Creating another DataFrame with this new dictionary from here above
-norm_std_dev_df = pd.DataFrame(norm_std_dev_WTdata)
-
-# Printing the results in DataFrame format
+# Printing the normalized standard deviations
 print("The normalized standard deviations calculated for each wind turbine are:")
-print(norm_std_dev_df)
+print(f'{norm_std_devs_df}')
+
+# Plotting absolute and normalized standard deviations in separate subplots
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12, 10))
+
+# Plotting absolute standard deviations
+std_devs_df.plot(kind='bar', ax=axes[0])
+axes[0].set_title('Absolute Standard Deviations of Output Metrics for Three Turbines')
+axes[0].set_xlabel('Turbine')
+axes[0].set_ylabel('Standard Deviation')
+axes[0].tick_params(axis='x', rotation=0)
+axes[0].legend(title='Metric')
+
+# Plotting normalized standard deviations for all WTs compared to WT Baseline
+
+norm_std_devs_df.plot(kind='bar', ax=axes[1])
+axes[1].set_title('Normalized Standard Deviations of Output Metrics for the 3 WTs')
+axes[1].set_xlabel('Turbine')
+axes[1].set_ylabel('Normalized Standard Deviation')
+axes[1].tick_params(axis='x', rotation=0)
+axes[1].legend(title='Metric')
+
+plt.tight_layout()
+plt.show()
